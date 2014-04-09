b463b63ceb9f849c97a4334dc9bfd7bc830bbe5a
Diriquel <diriquel@alumnos.ubiobio.cl>
2014-04-08 21:23:56 -0400

    Inicio de proyecto

---
+++ Main/src/Ventana.java
@@ -0,0 +1,13 @@
+
+import javax.swing.JFrame;
+
+public class Ventana {
+
+	public static void main(String[] args){
+		JFrame frame= new JFrame();
+		frame.setTitle("Mi ventana");
+		frame.setSize(800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	} 
+}
